2cbebe955ae10566eb49966883a200c317e1642c
Will Bryant <will.bryant@gmail.com>
2012-08-18 17:45:27 +1200

    move initializers for gem

--- lib/table_display.rb
+++ lib/table_display.rb
@@ -103,3 +103,12 @@ module Kernel
     puts target.to_table(*options)
   end
 end
+
+# all Enumerable classes should get TableDisplay functionality...
+Enumerable.send(:include, TableDisplay)
+
+# including those that have already included Enumerable by the time this plugin is loaded.
+# Ruby doesn't recursively update through the module tree, so although any new classes/modules
+# that include Enumerable will get TableDisplay, we have to do it ourself for older ones.
+ObjectSpace.each_object(Module) {|o| o.send(:include, TableDisplay) if o.ancestors.include?(Enumerable)}
+ObjectSpace.each_object(Class)  {|o| o.send(:include, TableDisplay) if o.ancestors.include?(Enumerable)}
