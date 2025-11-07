00b58f44d81860dd43eb7e4dffc85c40ddf93ba0
Will Bryant <will.bryant@gmail.com>
2025-11-07 13:51:09 +1300

    Update test startup to work with newer Rails versions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,8 +12,8 @@ FileUtils.mkdir File.join(File.dirname(__FILE__), "log") rescue nil
 RAILS_DEFAULT_LOGGER = ActiveRecord::Base.logger = Logger.new(File.join(File.dirname(__FILE__), "log", "#{ENV['RAILS_ENV']}.log"))
 
 ActiveRecord::Base.configurations = YAML::load(IO.read(File.join(File.dirname(__FILE__), "database.yml")))
-ActiveRecord::Base.establish_connection ActiveRecord::Base.configurations[ENV['RAILS_ENV']]
+ActiveRecord::Base.establish_connection ActiveRecord::Base.configurations.find_db_config(ENV['RAILS_ENV'])
 ActiveSupport::TestCase.send(:include, ActiveRecord::TestFixtures) if ActiveRecord.const_defined?('TestFixtures')
-ActiveSupport::TestCase.fixture_path = File.join(File.dirname(__FILE__), "fixtures")
+ActiveSupport::TestCase.fixture_paths = [File.join(File.dirname(__FILE__), "fixtures")]
 
 require File.expand_path(File.join(File.dirname(__FILE__), '../init')) # load table_display
